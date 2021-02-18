485fc3a867f74d5023f806133cbc02fed22dd418
Seyifunmi Gandonu <655453@student.inholland.nl>
2021-02-18 15:07:48 +0100

    GIT_TEST

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,10 @@ define('DB_DBNAME', 'sql11392316');
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
+//URLROOT
 define ('URLROOT', 'http://localhost/HaarlemFestivalG4');
+
+//SITENAME
 define('SITENAME', 'Haarlem Festival');
 
 
