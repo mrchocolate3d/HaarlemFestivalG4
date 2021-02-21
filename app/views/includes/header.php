3be0eddc3d674f74cbf14d43c76c56afe1ea8d60
Stephen Holden <25555894+Sholden8340@users.noreply.github.com>
2021-02-21 04:45:36 +0100

    Login button style Removed from list Changed login / logout to ternary operator

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -8,20 +8,18 @@
     <title><?php echo SITENAME; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?php echo URLROOT . "/public/css/style.css"?>">
+    <link rel="stylesheet" href="<?php echo URLROOT . "/public/css/style.css" ?>">
 
 </head>
+
+<?php
+// $_SESSION['user_id'] = 'test_user_id';
+?>
 
 <body>
 
     <nav class="nav">
-        <ul>
-            <li class="btn-login">
-                <?php if (isset($_SESSION['user_id'])) : ?>
-                    <a href="<?php echo URLROOT; ?>/app/index.php">Log out</a>
-                <?php else : ?>
-                    <a href="<?php echo URLROOT; ?>/app/index.php">Login</a>
-                <?php endif; ?>
-            </li>
-        </ul>
+        <a class="btn-login" href="<?php echo URLROOT; ?>/app/index.php">
+            <?php echo isset($_SESSION['user_id']) ?  "Log out" :  "Log In" ?>
+        </a>
     </nav>
