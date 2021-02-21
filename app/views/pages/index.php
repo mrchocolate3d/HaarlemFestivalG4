adc8f668c3e38086c66a11219bff420926f4e64a
Stephen Holden <25555894+Sholden8340@users.noreply.github.com>
2021-02-21 04:24:30 +0100

    Initial homepage layout

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,2 +1,52 @@
-<?php
-var_dump($data);
+<?php include APPROOT . '\views\includes\header.php'; ?>
+
+<main>
+    <?php //var_dump($data); 
+    ?>
+
+    <section id="home-title-screen">
+        <!-- <img id="fullscreen-image"src="<?php echo URLROOT ?>/public/img/jazz_home.png" alt="Haarlem Festival home"> -->
+        <h1>Haarlem Festival</h1>
+        <h2>Make the best out of your summer holiday</h2>
+        <h3>The Festival for everyone</h3>
+        <button class="transparent" name="arf">AHHHHHHHHHH</button>
+    </section>
+
+    <section class="container">
+
+        <h2>Experience Haarlem</h2>
+        <section id="home-info">
+            <h3>Who are we?</h3>
+            <p>
+                Haarlem has a rich history dating back to pre-medieval times. It is also one
+                of the most exciting and fun cities in the Netherlands featuring many museums,
+                art galleries, café shops, restaurants, and various other exciting things.
+            </p>
+            <p>
+                To keep the heritage live and make the world aware of such a wonderful
+                city, Haarlem festival is being organised in July 2021.This website is designed
+                to promote and offer the options to digitally explore the festival,
+                find information and buy tickets to the individual events performed during
+                this festival. The prominent events of Jazz-a versatile music programs
+                and dance-performed by world renown personalities are going to be the
+                key attractions. This festival has also been attraction for many international
+                visitors across the globe.
+            </p>
+            <p>
+                Looking forward to host you during the festival.
+                Click on the 'show more' button for more information to see exciting
+                events Haarlem festival has to offer.
+            </p>
+
+            <aside>
+                <img src="" alt="Dance Image">
+                <img src="" alt="History Image">
+                <img src="" alt="Jazz Image">
+            </aside>
+        </section>
+
+    </section>
+</main>
+
+
+<?php include APPROOT . '\views\includes/footer.php'; ?>
