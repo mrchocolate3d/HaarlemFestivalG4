2a6c6cb468a15d5b012a8be55b10dea8288e2a81
Seyifunmi Gandonu <655453@student.inholland.nl>
2021-02-24 20:16:15 +0100

    Homepage, Register page and Login page have been created with the links to each page being created. Register page is fully functional. Design needs to be done by the other group but the cms used now is only temporary.

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -7,10 +7,6 @@ require APPROOT . '/views/includes/header.php';
     require APPROOT . '/views/includes/navigation.php';
     ?>
 
-    <section class="wrapper-landing">
-        <h1>One man's crappy software</h1>
-        <h2>is another man's full-time job.</h2>
-    </section>
 </section>
 
 
