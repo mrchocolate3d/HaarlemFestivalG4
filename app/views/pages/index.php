5579e07d7645c1d9e22b07942aa996bd0e0edcf1
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2021-03-01 14:51:56 +0100

    Update dance view landing page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -23,6 +23,39 @@ require APPROOT . '/views/includes/header.php';
             </p>
             <button class="transparent" href="#">Show More</button>
     </section>
+
+    <section id="home-info-page">
+            <section id="dance-info">
+                <h3>Dance in Haarlem</h3>
+                <p>
+                    Haarlem is no stranger to dance with many localities and venues embracing it. 
+                </p>
+                <p>
+                    the event's sheer size impressive lineups and unbeatable party atomosphere will make your experience in dance like never before. 
+                </p>
+            </section>
+
+            <section id="theather-info">
+                    <h3>Caprera openluchttheather</h3>
+                    <p>
+                        caprera openluchttheather prounanced as (caprera open air theather in English) is the most popular and amazing location to experience any kind of performances. 
+                    </p>
+                    <p>
+                        Located between the dunes and forest, here you can enjoy all kinds of dance events experiencing the enchanting evening under the stars giving you unforgettable experieces
+                    </p>
+            </section>
+
+            <section id="theather2">
+                    <h3>XO the club</h3>
+                    <p>
+                        The second most popular club in the netherlands known for hosting incrediable performances 
+                        and want to dance the night away then you should definiately go to this club.
+                    </p>
+                    <p>
+                        the doors are open from 9pm at night to 4 am in the morning!
+                    </p>
+            </section>
+    </section>
 </main>
 
 
