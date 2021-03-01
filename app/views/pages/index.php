cc9159f637e8864f5611300e42e74a97f4b40e70
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2021-03-01 13:53:53 +0100

    Update dance view page

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,15 +2,27 @@
 require APPROOT . '/views/includes/header.php';
  ?>
 
-<section id="section-landing">
+<!-- <section id="section-landing">
     <?php
-    require APPROOT . '/views/includes/navigation.php';
-    ?>
+    //require APPROOT . '/views/includes/navigation.php';
+    ?> 
 
     <section class="wrapper-landing">
         <h1>One man's crappy software</h1>
         <h2>is another man's full-time job.</h2>
     </section>
-</section>
+</section> -->
+
+<main id="Dancehome">
+    <section id="Dance-title">
+            <h1>Haarlem Dance</h1>
+            <p>
+                The festival bringing you to the best dance acts from, both local and all around the world.performing at haarlem's very own 
+                caprera openluchttheather, and other exciting theathers in haarlem, a enjoyable
+                and fun event you dont wanna miss. 
+            </p>
+            <button class="transparent" href="#">Show More</button>
+    </section>
+</main>
 
 
