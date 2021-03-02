828a515540065307fe12e0ec740d74afc7bd83aa
sam0111 <54842705+sam0111@users.noreply.github.com>
2021-03-02 23:03:44 +0100

    History
    CSS and small changes in the detail page

--- app/views/history/detail.php
+++ app/views/history/detail.php
@@ -13,35 +13,39 @@
         ?>
     </section>
 
-    <section class="main-container">
-        <aside class="info-container">
-            <h2>Haarlem History</h2>
-            <p>Haarlem history gives you a unique experience of Haarlem,
-                it is something you do not want to miss.
-                Join one of our tours on Thursday, Friday, Saturday
+    <section class="main-history-container">
+        <aside class="intro-history-container">
+            <h1 id="history-intro-h">Haarlem History</h1>
+            <p id="history-intro">Haarlem history gives you a unique experience of Haarlem,
+                <br>it is something you do not want to miss.
+                <br>Join one of our tours on Thursday, Friday, Saturday
                 or Sunday.</p>
-            <button type="button">Join us</button>
+            <button type="button" id="history-show-more">Join us</button>
         </aside>
     </section>
 
-    <section class="detail-container">
-        <section class="info-container">
-            <h2>Tour Information</h2>
-            <p>Our tours are for anyone to enjoy, it is especially a way for some quality family time.
-                It is available in Chinese, Dutch and English on Thursday, Friday, Saturday and Sunday.</p>
-            <p>All our tours start at the location "De Grote of St.-Bavokerk", the guides will make sure you're experiencing the best of Haarlem.
-            </p>
-            <p>We only have 12 seats per tour so make sure you book your tickets on time.
-            </p>
-            <p>Standard tickets: 17,50 euros
-                <br>Family tickets for groups of four: 60,00 euros. </p>
+    <section class="detail-history-container">
+        <h2 id="history-detail-h">Travel through history with us</h2>
+        <section class="tour-history-container">
+            <section class="tour-text-cont">
+                <h1 id="tour-title">Tour Information</h1>
+                <p>Our tours are for anyone to enjoy, it is especially a way for some quality family time.
+                    It is available in Chinese, Dutch and English on Thursday, Friday, Saturday and Sunday.</p>
+                <p>All our tours start at the location "De Grote of St.-Bavokerk", the guides will make sure you're experiencing the best of Haarlem.
+                </p>
+                <p>We only have 12 seats per tour so make sure you book your tickets on time.
+                </p>
+                <p>Standard tickets: 17,50 euros
+                    <br>Family tickets for groups of four: 60,00 euros. </p>
 
-            <button type="button">Order tickets</button>
+                <button type="button">Order tickets</button>
+            </section>
+            <aside id="history-map-aside">
+                <button onclick="showPopUp()" id = popup>Show locations</button>
+            </aside>
 
         </section>
-        <aside>
-            <button onclick="showPopUp()" id = popup>Show locations</button>
-        </aside>
+
 
 
     </section>
