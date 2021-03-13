e6abf0616e9e15098345d1c989a1a80ebd8e2bbb
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2021-03-13 19:49:53 +0100

    popup update.

--- app/views/dance/home.php
+++ app/views/dance/home.php
@@ -10,7 +10,7 @@
             $vat = 0.21 * $price;
             $totalprice = $price + $vat;
             $popup = '<section id="Modal" class="modal">
-                    <!-- Modal content -->
+                        <!-- Modal content -->
                         <section class="modal-content">
                             <span class="close">&times;</span>
                             <article id="modal-artist">
@@ -94,8 +94,47 @@
 
                         </section>
                     </section>';
+                    $popup_script = "<script>
+                    $(function() {
+                        // CODE TO CLOSE THE POPUP
+                        // USE THE .on METHOD IN CASE WE
+                        // WANT TO MODIFY THIS TO LOAD POPUP
+                        // CONTENT VIA AJAX
+                        $('body').on('click','.closePopup', function() {
+                            $('.modal').remove()
+                        });
+                        // HANDLE THE WINDOW RESIZE.
+                        // WHEN WINDO IS RESIZED - MAKE SURE
+                        // POPUP STAYS CENTERED.
+                        $(window).resize(function() {
+                            // FIND THE POPUP
+                            var popup = $('#Modal');
+                        // IF IT EXISTS CENTRE IT
+                            if (popup.length > 0) {
+                            centerPopup();
+                            }
+                        });
+                        
+                        $('document').ready(function(e) {
+                            var popup = '".$popup."';
+                            $('body').append(popup);
+                            centerPopup();
+                        });
+                        });
+                        
+                        function centerPopup()
+                        {
+                        var popup = $('#Modal');
+                        // LEFT AND TOP VALUES IS HALF THE DIFFERENCE 
+                        // BETWEEN THE WINDOW AND POPUP METRICS.
+                        // USE THE SHIFT RIGHT OPERATOR TO DO DIV BY 2
+                        var left = ($(window).width() - popup.width()) >> 1;
+                        var top = ($(window).height() - popup.height()) >> 1;
+                        // SET LEFT AND TOP STYLES TO CALCULATED VALUES
+                        popup.css({left: left + 'px', top: top + 'px'});
+                        }";
 
-                    echo $popup;
+                    echo $popup_script;
             }
     }
     // if user clicks add to cart this will be evaluated 
@@ -103,7 +142,83 @@
         
     }
 ?>
+    <script type="text/javascript">
+            $(function() {
+            // CODE TO CLOSE THE POPUP
+            // USE THE .on METHOD IN CASE WE
+            // WANT TO MODIFY THIS TO LOAD POPUP
+            // CONTENT VIA AJAX
+            $('body').on('click','.closePopup', function() {
+                // CHANGE BACKGROUND TO GREEN 
+            // FOLLOWED BY A FADEOUT TO GIVE
+            // A DELAY TO SHOW CHANGE IN COLOUR
+                $('.action input').css({backgroundColor: 'green'}).fadeOut(300, function() {
+                // REMOVE ALL ELEMENTS WITH THE 
+                // popupElement STYLE - INCLUDES OVERLAY
+                // AND POUP
+                $('.popupElement').remove()
+                });
+            });
+            // HANDLE THE WINDOW RESIZE.
+            // WHEN WINDO IS RESIZED - MAKE SURE
+            // POPUP STAYS CENTERED.
+            $(window).resize(function() {
+                // FIND THE POPUP
+                var popup = $('#popupWindow');
+            // IF IT EXISTS CENTRE IT
+                if (popup.length > 0) {
+                centerPopup();
+                }
+            });
+            // TRIGER DISPLAY OF POPUP
+            $('a').click(function(e) {
+                // DISABLE DEFAULT CLICK FUNCTIONALITY FOR <a>
+                e.preventDefault();
+            // CREATE OUR OVERLAY AND APPEND TO BODY
+                var overlay = $('<div/>').addClass('overlay').addClass('popupElement');
+                $('body').append(overlay);
+            // CREATE OUR POPUP AND POSITION OFFSCREEN.
+            // WE DO THIS SO WE CAN DISPLAY IT AND CALCULATE
+            // ITS WIDTH AND HEIGHT SO WE CAN CENTRE IT
+                var popup = $('<div/>').attr('id','popupWindow').addClass('popup').addClass('popupElement').css({left: '-999px'});
+            // CREATE THE HTML FOR THE POPUP
+                var html = '<img src="' + $(this).attr('href') + '" /><div class="action"><input type="button" value="Continue" class="closePopup"/></div>';
+                popup.html(html);
+            // APPEND THE POPUP TO THE BODY
+                $('body').append(popup);
+            // AND CENTER IT
+                centerPopup();
+            });
+            });
+            // FUNCTION TO CENTER THE POPUP
+            function centerPopup()
+            {
+            var popup = $('#popupWindow');
+            // LEFT AND TOP VALUES IS HALF THE DIFFERENCE 
+            // BETWEEN THE WINDOW AND POPUP METRICS.
+            // USE THE SHIFT RIGHT OPERATOR TO DO DIV BY 2
+            var left = ($(window).width() - popup.width()) >> 1;
+            var top = ($(window).height() - popup.height()) >> 1;
+            // SET LEFT AND TOP STYLES TO CALCULATED VALUES
+            popup.css({left: left + 'px', top: top + 'px'});
+            }
+</script>
+<style type="text/css">
+.modal {
+  background: #999;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  opacity: 0.95;
+  filter: alpha(opacity=95);
+  z-index: 1;
+}
+</style>
+
 <main>
+    <a href="images/a3.jpg">Click me</a>
     <section class="title-screen">
        
         <section id="dance-title">
@@ -197,6 +312,7 @@
                $table.= '</table>';
                echo $table;
         ?>
+        
         <script>
             // Get the modal
             var modal = document.getElementById("Modal");
