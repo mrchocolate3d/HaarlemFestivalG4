964a8241def5f908c71087b2b9c06727881bf23c
sam0111 <54842705+sam0111@users.noreply.github.com>
2021-04-04 17:04:36 +0200

    payment

--- vendor/guzzlehttp/guzzle/src/Exception/TransferException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace _PhpScoperf97ee63196d1\GuzzleHttp\Exception;
-
-class TransferException extends \RuntimeException implements \_PhpScoperf97ee63196d1\GuzzleHttp\Exception\GuzzleException
-{
-}
--- vendor/guzzlehttp/guzzle/src/functions_include.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace _PhpScoperf97ee63196d1;
-
-// Don't redefine the functions if included multiple times.
-if (!\function_exists('_PhpScoperf97ee63196d1\\GuzzleHttp\\uri_template')) {
-    require __DIR__ . '/functions.php';
-}
