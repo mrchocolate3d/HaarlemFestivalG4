4105c58af61eaa64fb47201adc20be574cdb8be7
sam0111 <54842705+sam0111@users.noreply.github.com>
2021-04-05 18:47:10 +0200

    payment necessary stuff

--- app/controllers/Payments.php
+++ app/controllers/Payments.php
@@ -1,17 +1,37 @@
 <?php
 
+use Mollie\Api\MollieApiClient;
+require "../initialize.php";
 
 class Payments extends Controller
 {
     public function __construct()
     {
         $this->paymentModel = $this->model('Payment');
+        $this->userModel =$this->model('User');
+
     }
     public function payment()
     {
+        $mollie = new MollieApiClient();
         $this->view('payments/payment');
         if(isset($_POST['pay'])){
-            $method = $_POST('');
+
+            $mollie->setApiKey('test_ajQEAHf8StBWg3dnW9VxWvcz26j5jh');
+            $amount = $_POST['amount'];
+
+            $payment = $mollie->payments->create([
+                "amount" => [
+                    "currency" => "EUR",
+                    "value" => "10.00",
+                ],
+                "description" => "Order #{$orderId}",
+                "redirectUrl" => "{$protocol}://{$hostname}{$path}/return.php?order_id={$orderId}",
+                "webhookUrl" => "{$protocol}://{$hostname}{$path}/webhook.php",
+                "metadata" => [
+                    "order_id" => $orderId,
+                ],
+            ]);
             $this->paymentModel->placeOrder();
         }
     }
